18dfbfb56bc8ed4e3b4c53bb09d6f2da3517f3a6
RichardSK0 <krajcik17@stud.uniza.sk>
2025-11-18 17:52:55 +0100

    nove triedy test

---
+++ projekt/Mesiac.java
@@ -0,0 +1,10 @@
+
+/**
+ * Write a description of class Mesiac here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Mesiac
+{
+}
---
+++ projekt/Teren.java
@@ -0,0 +1,10 @@
+
+/**
+ * Write a description of class Teren here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Teren
+{
+}
